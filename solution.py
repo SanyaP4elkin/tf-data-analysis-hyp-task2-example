3d1cc1a14d5fc7f1890403902a3ef09cb41c1c14
SanyaP4elkin <104874172+SanyaP4elkin@users.noreply.github.com>
2023-04-13 00:33:08 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import anderson_ksamp
 
 
 chat_id = 123456 # Ваш chat ID, не меняйте название переменной
@@ -8,4 +9,8 @@ def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    res = anderson_ksamp([x,y])
+    if res[2] < 0.07:
+        return True
+    else:
+        return False # Ваш ответ, True или False
